e1fa9cc06bffa3e629c581b811fa87382a1b7722
hnamvuong <hnam.vuong@gmail.com>
2019-08-09 13:59:11 +0700

    feat(webapp): add asset file

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+	.js('resources/js/bootstrap.js', 'public/js')
+	.js('resources/js/myjs.js', 'public/js/myjs.js')
+	.js('resources/js/js.js', 'public/js/js.js')
+	.js('resources/js/webfont.js', 'public/js/webfont.js')
+	.js('resources/js/jquery.min.js', 'public/js/jquery.min.js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .copyDirectory('resources/css/mystyle.css', 'public/css/mystyle.css')
+    .copyDirectory('resources/css/style.css', 'public/css/style.css');
